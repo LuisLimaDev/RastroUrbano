4b1b976c8ef947b482c3988e73813f15f0b1f7de
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2020-05-11 13:59:30 -0300

    Add files via upload

---
+++ portfolio/portfolio.js
@@ -0,0 +1,45 @@
+// portifolio.js
+{
+	trabalhos = [
+		[{
+			"nome" : "DEIXA EU DIZER",
+			"artista" : "BIG FIELD COMPANY",
+			"capa" : "media/videoClip.WMBeats.DeixaEuDizer/capa.png",
+			"icone" : "media/videoClip.WMBeats.DeixaEuDizer/capa.png",
+			"link": "http://localhost/rastro/portifolio/?trabalho=DeixaEuDizer",
+			"conteudo" : "p = Vídeo Clipe - DEIXA EU DIZER (WMbeats) ;img = media/videoClip.WMBeats.DeixaEuDizer/capa.png ;a.botao = https://www.behance.net/gallery/78358901/Video-Clipe-e-Arte-DEIXA-EU-DIZER-%28WMbeats%29 ;p = Direção de Fotografia: Bruno Moura ;p = Edição de Vídeo: Paulo Gomes;"
+		}],
+		[{
+			"nome" : "PRÊMIO R.U.A.",
+			"artista" : "RASTRO URBANO AGENCY",
+			"capa" : "media/premioRUA/capa.png",
+			"icone" : "media/premioRUA/capa.png",
+			"link": "?trabalho=PremioRUA",
+			"conteudo" : "img = media/premioRUA/capa.png"
+		}],
+		[{
+			"nome" : "VIDEO CLIP",
+			"artista" : "NYAK",
+			"capa" : "media/capa.nyak.naoDeixeORioLevar/capa.png",
+			"icone" : "media/capa.nyak.naoDeixeORioLevar/capa.png",
+			"link": "?trabalho=NaoDeixeORioLevar",
+			"conteudo" : "img = media/capa.nyak.naoDeixeORioLevar/portnyak-1.png ;img = media/capa.nyak.naoDeixeORioLevar/portnyak-2.png ;img = media/capa.nyak.naoDeixeORioLevar/portnyak-3.png"
+		}],
+		[{
+			"nome" : "IDENTIDADE VISUAL",
+			"artista" : "RASTRO URBANO AGENCY",
+			"capa" : "media/idVisual.Rastro/capa.png",
+			"icone" : "media/idVisual.Rastro/capa.png",
+			"link": "http://localhost/rastro/portifolio/?trabalho=IDENTIDADEdaRatro",
+			"conteudo" : "img = media/idVisual.Rastro/idv01.png ;img = media/idVisual.Rastro/idv02.png ;img = media/idVisual.Rastro/idv03.png"
+		}],
+		[{
+			"nome" : "ARTE DE ALBUM",
+			"artista" : "2020 - G.H.",
+			"capa" : "media/ep.gh.2020/capa.png",
+			"icone" : "media/ep.gh.2020/capa.png",
+			"link": "http://localhost/rastro/portifolio/media/ep.gh.2020/capa.png",
+			"conteudo" : "youtube = https://www.youtube.com/embed/tFTgBwYMw_4 ;youtube = https://www.youtube.com/embed/327ppCmm9og ;youtube = https://www.youtube.com/embed/RFgZ0C-PU8I ;youtube = https://www.youtube.com/embed/3YKwNG7Dq6A ;p = Produção Visual do EP 2020 - GH ;p = Rastro Urbano Agência de Design e Fotografia ;p = Foto e animação: Bruno Moura ;p = Direção de Arte e capa: Paulo Gomes "
+		}]
+	]
+}
---
+++ videos.js
@@ -0,0 +1,34 @@
+// videos.js
+//
+videos = [
+	[{
+		"nome": "GH - 01. Carrossel da Vida (EP 2020)",
+		"link": "https://www.youtube.com/embed/tFTgBwYMw_4",
+		"capa": "https://i.ytimg.com/vi/ofQX7RMbVEU/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCOmVlbSCQiid05fPtGb8kS_fLhgg"
+	}],
+	[{
+		"nome": "BFCO - De onde eu vim",
+		"link": "https://www.youtube.com/embed/ofQX7RMbVEU",
+		"capa": "https://i.ytimg.com/vi/ofQX7RMbVEU/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCOmVlbSCQiid05fPtGb8kS_fLhgg"
+	}],
+	[{
+		"nome": "Nyak - Não Deixe o Rio Levar (Audio Oficial)",
+		"link": "https://www.youtube.com/embed/gLzMmlX-3W4",
+		"capa": "https://i.ytimg.com/vi/Z6mE2fMgDGE/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAmGCqW_AMQEvbNqyAp5oOAZjWafQ"
+	}],
+	[{
+		"nome": "WM Beats - Deixa Eu Dizer part. Dustin e Welivelton",
+		"link": "https://www.youtube.com/embed/SVJ1-uEM0Rk",
+		"capa": "https://i.ytimg.com/vi/SVJ1-uEM0Rk/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD0eIM7hyxArwCV2VKvsR1_sBsvvQ"
+	}],
+	[{
+		"nome": "Big Field Company (NY AK e Dustin Maia): Fresta | Selo Fôlego",
+		"link": "https://www.youtube.com/embed/7MfUjOVjQh8",
+		"capa": "https://i.ytimg.com/vi/7MfUjOVjQh8/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLBjcViyQAAHbDHpQfHXTd6JEcxL3w"
+	}],
+	[{
+		"nome": "Banda Publica - O Que é Cultura?",
+		"link": "https://www.youtube.com/embed/hhB-X2SZmHo",
+		"capa": "https://i.ytimg.com/vi/hhB-X2SZmHo/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAn2wZ_ZWeWygVRJmxlmSr3hV6pPA"
+	}]
+]
