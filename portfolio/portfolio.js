d36d00f47ba58ea11b2cebded7f67a691caf6d4e
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2020-05-16 17:36:07 -0300

    Update portfolio.js

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -31,15 +31,15 @@
 			"capa" : "media/idVisual.Rastro/capa.png",
 			"icone" : "media/idVisual.Rastro/capa.png",
 			"link": "http://localhost/rastro/portifolio/?trabalho=IDENTIDADEdaRatro",
-			"conteudo" : "img = media/idVisual.Rastro/idv01.png ;img = media/idVisual.Rastro/idv02.png ;img = media/idVisual.Rastro/idv03.png"
+			"conteudo" : "img = media/idVisual.Rastro/idv01.png ;img = media/idVisual.Rastro/idv02.png ;img = media/idVisual.Rastro/idv03.png ;img = media/idVisual.Rastro/idv04.png ;img = media/idVisual.Rastro/idv05.gif ;img = media/idVisual.Rastro/idv06.png ;img = media/idVisual.Rastro/idv07.png ;img = media/idVisual.Rastro/idv08.png ;img = media/idVisual.Rastro/idv09.png ;img = media/idVisual.Rastro/idv10.png ;img = media/idVisual.Rastro/idv11.png ;img = media/idVisual.Rastro/idv12.png ;img = media/idVisual.Rastro/idv13.png"
 		}],
 		[{
 			"nome" : "ARTE DE ALBUM",
 			"artista" : "2020 - G.H.",
-			"capa" : "media/ep.gh.2020/capa.png",
+			"capa" : "media/ep.gh.2020/miniatura.jpeg",
 			"icone" : "media/ep.gh.2020/capa.png",
 			"link": "http://localhost/rastro/portifolio/media/ep.gh.2020/capa.png",
-			"conteudo" : "youtube = https://www.youtube.com/embed/tFTgBwYMw_4 ;youtube = https://www.youtube.com/embed/327ppCmm9og ;youtube = https://www.youtube.com/embed/RFgZ0C-PU8I ;youtube = https://www.youtube.com/embed/3YKwNG7Dq6A ;p = Produção Visual do EP 2020 - GH ;p = Rastro Urbano Agência de Design e Fotografia ;p = Foto e animação: Bruno Moura ;p = Direção de Arte e capa: Paulo Gomes "
+			"conteudo" : "img = media/ep.gh.2020/capa.png ;youtube = https://www.youtube.com/embed/tFTgBwYMw_4 ;youtube = https://www.youtube.com/embed/327ppCmm9og ;youtube = https://www.youtube.com/embed/RFgZ0C-PU8I ;youtube = https://www.youtube.com/embed/3YKwNG7Dq6A ;p = Produção Visual do EP 2020 - GH ;p = Rastro Urbano Agência de Design e Fotografia ;p = Foto e animação: Bruno Moura ;p = Direção de Arte e capa: Paulo Gomes "
 		}]
 	]
 }
