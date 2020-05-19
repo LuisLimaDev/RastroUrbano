7bd147871ffdd36e284564846752f22a85e93a10
LuisLimaDev <56639074+LuisLimaDev@users.noreply.github.com>
2020-05-19 14:12:53 -0300

    Add files via upload

---
+++ videos/videos.js
@@ -0,0 +1,39 @@
+// videos.js
+//
+videos = [
+	[{
+		"nome": "Nyak - Não Deixe o Rio Levar (Audio Oficial)",
+		"link": "https://www.youtube.com/embed/gLzMmlX-3W4",
+		"capa": "https://i.ytimg.com/vi/Z6mE2fMgDGE/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAmGCqW_AMQEvbNqyAp5oOAZjWafQ"
+	}],
+	[{
+		"nome": "Batalha da Rampa #3 | ELIMINATÓRIAS - AZEC × VENNOM",
+		"link": "https://www.youtube.com/embed/jswQ1rItUqk",
+		"capa": "https://i.ytimg.com/vi/jswQ1rItUqk/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCOmVlbSCQiid05fPtGb8kS_fLhgg"
+	}],
+	[{
+		"nome": "Batalha da Rampa #4 | ELIMINATÓRIAS DOUBLE THREE - DI × SAMU × JHONN",
+		"link": "https://www.youtube.com/embed/3qSw6XLPnVc",
+		"capa": "https://i.ytimg.com/vi/3qSw6XLPnVc/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCOmVlbSCQiid05fPtGb8kS_fLhgg"
+	}],
+	[{
+		"nome": "Batalha da Rampa #5 | ELIMINATÓRIAS - NIVI × 2T",
+		"link": "https://www.youtube.com/embed/0WBDeM4AM1E",
+		"capa": "https://i.ytimg.com/vi/0WBDeM4AM1E/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAmGCqW_AMQEvbNqyAp5oOAZjWafQ"
+	}],
+	[{
+		"nome": "Batalha da Rampa #6 | ELIMINATÓRIAS - BOBIS × ALEXT",
+		"link": "https://www.youtube.com/embed/rzUXW_pY3OQ",
+		"capa": "https://i.ytimg.com/vi/rzUXW_pY3OQ/hqdefault.jpg?sqp=-oaymwEZCPYBEIoBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLD0eIM7hyxArwCV2VKvsR1_sBsvvQ"
+	}],
+	[{
+		"nome": "Batalha da Rampa #7 | ELIMINATÓRIAS - SAMU × ALEXT",
+		"link": "https://www.youtube.com/embed/XIW21QrLoFQ",
+		"capa": "https://i.ytimg.com/vi/XIW21QrLoFQ/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAn2wZ_ZWeWygVRJmxlmSr3hV6pPA"
+	}],
+	[{
+		"nome": "Batalha da Rampa #8 | FINAL DOUBLE THREE - NIVI ×VENNOM × SAMU",
+		"link": "https://www.youtube.com/embed/S9ukb3iEjEA",
+		"capa": "https://i.ytimg.com/vi/S9ukb3iEjEA/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLAn2wZ_ZWeWygVRJmxlmSr3hV6pPA"
+	}]
+]
